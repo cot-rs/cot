f2f3aea2be88f2e9a89687b22a460d246a96602e
Elijah <elijahahianyo@gmail.com>
2025-06-19 19:44:37 +0000

    remove unnecessary test name demarcation

--- cot/src/common_types.rs
+++ cot/src/common_types.rs
@@ -650,9 +650,6 @@ mod tests {
 
     use super::*;
 
-    // ------------------------
-    // Password tests
-    // ------------------------
     #[test]
     fn url_new() {
         let parse_url = Url::new("https://example.com/").unwrap();
@@ -679,9 +676,7 @@ mod tests {
         assert_eq!(password.as_str(), "password");
         assert_eq!(password.into_string(), "password");
     }
-    // ------------------------
-    // Email tests
-    // ------------------------
+
     #[test]
     fn test_valid_email_creation() {
         let email = Email::new("user@example.com").unwrap();
@@ -711,9 +706,7 @@ mod tests {
     mod db_tests {
         use super::*;
         use crate::db::ToDbValue;
-        // ------------------------
-        // Email tests
-        // ------------------------
+
         #[test]
         fn test_to_db_value() {
             let email = Email::new("user@example.com").unwrap();

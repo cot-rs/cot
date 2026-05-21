ae1e5337b84ffb9b98edb48fa0d5a9e0275ede50
Mateusz Maćkowski <m4tx@m4tx.pl>
2026-05-21 15:58:09 +0200

    better test

--- cot/src/auth/db.rs
+++ cot/src/auth/db.rs
@@ -115,19 +115,38 @@ impl DatabaseUser {
 
     /// Sets the username of the user.
     ///
+    /// Remember to call [`save()`][cot::db::Model::save] after calling this
+    /// function - otherwise, the username will never be saved to the database.
+    ///
     /// # Example
     ///
     /// ```
-    /// use cot::auth::db::DatabaseUser;
-    /// use cot::db::LimitedString;
-    /// # use cot::common_types::Password;
-    /// # use cot::db::Auto;
+    /// use cot::auth::db::{DatabaseUser, DatabaseUserCredentials};
+    /// use cot::common_types::Password;
+    /// use cot::db::{Database, LimitedString, Model};
+    /// use cot::html::Html;
     ///
-    /// # let mut user = DatabaseUser::new(Auto::fixed(1), LimitedString::new("oldname").unwrap(), &Password::new("password"));
-    /// user.set_username(LimitedString::new("newname").unwrap());
+    /// async fn view(db: Database) -> cot::Result<Html> {
+    ///     let mut user =
+    ///         DatabaseUser::create_user(&db, "testuser".to_string(), &Password::new("password123"))
+    ///             .await?;
+    ///     user.set_username(LimitedString::new("new_username").unwrap());
+    ///     user.save(&db).await?;
     ///
-    /// use cot::auth::User;
-    /// assert_eq!(User::username(&user).unwrap(), "newname");
+    ///     assert_eq!(user.username(), "new_username");
+    ///
+    ///     Ok(Html::new("Username changed!"))
+    /// }
+    ///
+    /// # #[tokio::main]
+    /// # async fn main() -> cot::Result<()> {
+    /// #     use cot::test::{TestDatabase, TestRequestBuilder};
+    /// #     let mut test_database = TestDatabase::new_sqlite().await?;
+    /// #     test_database.with_auth().run_migrations().await;
+    /// #     view(test_database.database()).await?;
+    /// #     test_database.cleanup().await?;
+    /// #     Ok(())
+    /// # }
     /// ```
     pub fn set_username(&mut self, username: LimitedString<MAX_USERNAME_LENGTH>) {
         self.username = username;
@@ -135,17 +154,43 @@ impl DatabaseUser {
 
     /// Sets the password of the user.
     ///
+    /// Remember to call [`save()`][cot::db::Model::save] after calling this
+    /// function - otherwise, the password will never be saved to the database.
+    ///
     /// The password will be automatically hashed.
     ///
     /// # Example
     ///
     /// ```
-    /// use cot::auth::db::DatabaseUser;
+    /// use cot::auth::db::{DatabaseUser, DatabaseUserCredentials};
     /// use cot::common_types::Password;
-    /// # use cot::db::{Auto, LimitedString};
+    /// use cot::db::{Database, Model};
+    /// use cot::html::Html;
     ///
-    /// # let mut user = DatabaseUser::new(Auto::fixed(1), LimitedString::new("user").unwrap(), &Password::new("oldpassword"));
-    /// user.set_password(&Password::new("newpassword"));
+    /// async fn view(db: Database) -> cot::Result<Html> {
+    ///     let mut user =
+    ///         DatabaseUser::create_user(&db, "testuser".to_string(), &Password::new("password123"))
+    ///             .await?;
+    ///     user.set_password(&Password::new("new_password"));
+    ///     user.save(&db).await?;
+    ///
+    ///     let credentials =
+    ///         DatabaseUserCredentials::new(String::from("testuser"), Password::new("new_password"));
+    ///     let authenticated = DatabaseUser::authenticate(&db, &credentials).await?;
+    ///     assert!(authenticated.is_some());
+    ///
+    ///     Ok(Html::new("Password changed!"))
+    /// }
+    ///
+    /// # #[tokio::main]
+    /// # async fn main() -> cot::Result<()> {
+    /// #     use cot::test::{TestDatabase, TestRequestBuilder};
+    /// #     let mut test_database = TestDatabase::new_sqlite().await?;
+    /// #     test_database.with_auth().run_migrations().await;
+    /// #     view(test_database.database()).await?;
+    /// #     test_database.cleanup().await?;
+    /// #     Ok(())
+    /// # }
     /// ```
     pub fn set_password(&mut self, password: &Password) {
         self.password = PasswordHash::from_password(password);

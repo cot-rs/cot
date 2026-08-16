6590dc12dae87f7dc1d34786669b70352016c797
Elijah <elijahahianyo@gmail.com>
2026-08-16 23:45:13 +0000

    see if windows test gets fixed

--- cot-cli/tests/snapshot_testing/external/check.rs
+++ cot-cli/tests/snapshot_testing/external/check.rs
@@ -3,14 +3,14 @@ use insta_cmd::assert_cmd_snapshot;
 
 use crate::snapshot_testing::{GENERIC_FILTERS, TEMP_PATH_FILTERS, cot_cli_path, cot_cmd_in};
 
-// #[test]
-// fn check_forwards_to_project_binary() {
-//     let project = standard_project(cot_cli_path()).unwrap();
-//     insta::with_settings!(
-//         { filters => GENERIC_FILTERS.to_owned() },
-//         { assert_cmd_snapshot!(project.cot_cmd(&["check"])) }
-//     );
-// }
+#[test]
+fn check_forwards_to_project_binary() {
+    let project = standard_project(cot_cli_path()).unwrap();
+    insta::with_settings!(
+        { filters => GENERIC_FILTERS.to_owned() },
+        { assert_cmd_snapshot!(project.cot_cmd(&["check", "--build"])) }
+    );
+}
 
 #[test]
 fn double_dash_delimiter_fails_with_unsupported_flag_name() {
@@ -44,7 +44,7 @@ fn unrecognized_command_reports_unknown_command() {
     let project = standard_project(cot_cli_path()).unwrap();
     insta::with_settings!(
         { filters => GENERIC_FILTERS.to_owned() },
-        { assert_cmd_snapshot!(project.cot_cmd(&["banana"])) }
+        { assert_cmd_snapshot!(project.cot_cmd(&["banana", "--build"])) }
     );
 }
 
@@ -53,6 +53,6 @@ fn check_with_no_project_binary_reports_build_hint() {
     let tempdir = tempfile::TempDir::new().unwrap();
     insta::with_settings!(
         { filters => [GENERIC_FILTERS, TEMP_PATH_FILTERS].concat() },
-        { assert_cmd_snapshot!(cot_cmd_in(&["check"], tempdir.path())) }
+        { assert_cmd_snapshot!(cot_cmd_in(&["check", "--build"], tempdir.path())) }
     );
 }

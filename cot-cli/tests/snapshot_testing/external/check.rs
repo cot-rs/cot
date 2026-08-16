ffe7e722dcafb42d60cc1d909e5d9f2fe8ecaa8a
Elijah <elijahahianyo@gmail.com>
2026-08-16 23:11:14 +0000

    isolating tests

--- cot-cli/tests/snapshot_testing/external/check.rs
+++ cot-cli/tests/snapshot_testing/external/check.rs
@@ -3,14 +3,14 @@ use insta_cmd::assert_cmd_snapshot;
 
 use crate::snapshot_testing::{GENERIC_FILTERS, TEMP_PATH_FILTERS, cot_cli_path, cot_cmd_in};
 
-#[test]
-fn check_forwards_to_project_binary() {
-    let project = standard_project(cot_cli_path()).unwrap();
-    insta::with_settings!(
-        { filters => GENERIC_FILTERS.to_owned() },
-        { assert_cmd_snapshot!(project.cot_cmd(&["check"])) }
-    );
-}
+// #[test]
+// fn check_forwards_to_project_binary() {
+//     let project = standard_project(cot_cli_path()).unwrap();
+//     insta::with_settings!(
+//         { filters => GENERIC_FILTERS.to_owned() },
+//         { assert_cmd_snapshot!(project.cot_cmd(&["check"])) }
+//     );
+// }
 
 #[test]
 fn double_dash_delimiter_fails_with_unsupported_flag_name() {

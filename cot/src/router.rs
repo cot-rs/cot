5da212b63742aba45a6daa6f1b1d5eacded441ae
S.Dharshan <me.dharshan.1@gmail.com>
2026-08-19 22:26:54 +0530

    Revert "feat(router): add precedence based routing"
    This reverts commit 77ea6cae13ce63fa9022613ea084f2da46f0411a.

--- cot/src/router.rs
+++ cot/src/router.rs
@@ -36,7 +36,7 @@ use tracing::debug;
 use crate::error::NotFound;
 use crate::request::{PathParams, Request, RequestExt, RequestHead};
 use crate::response::Response;
-use crate::router::path::{CaptureResult, PathMatcher, ReverseParamMap, compare_weights};
+use crate::router::path::{CaptureResult, PathMatcher, ReverseParamMap};
 use crate::{Error, ProjectContext, Result};
 
 pub mod method;
@@ -102,9 +102,7 @@ impl Router {
     /// ```
     #[must_use]
     pub fn with_urls<T: Into<Vec<Route>>>(urls: T) -> Self {
-        let mut urls = urls.into();
-        urls.sort_by(|a, b| compare_weights(&a.url.token_weights(), &b.url.token_weights()));
-
+        let urls = urls.into();
         let mut names = HashMap::new();
 
         for url in &urls {

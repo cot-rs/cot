6ee0d9f6ce29bf12f00bff7199bfd81a75366ef4
Elijah <elijahahianyo@gmail.com>
2026-08-25 16:57:47 +0000

    use expect. move the macro to the method

--- cot-macros/src/from_request.rs
+++ cot-macros/src/from_request.rs
@@ -43,9 +43,9 @@ pub(super) fn impl_from_request_head_for_struct(
     };
 
     quote! {
-        #[allow(clippy::unused_async_trait_impl, clippy::unused_async)]
         #[automatically_derived]
         impl #cot::request::extractors::FromRequestHead for #struct_name {
+            #[expect(clippy::unused_async_trait_impl, clippy::unused_async)]
             async fn from_request_head(
                 head: &#cot::request::RequestHead,
             ) -> #cot::Result<Self> {
